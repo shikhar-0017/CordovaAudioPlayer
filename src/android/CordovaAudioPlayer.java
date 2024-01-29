9286dbb0d41a0dcb439d1c3ea0590d0fb8767bcc
Shikhar Shrivastav <shikhar.s@kiddopia.com>
2024-01-29 17:59:42 +0530

    Added android methods (preload, play, stop)

--- src/android/CordovaAudioPlayer.java
+++ src/android/CordovaAudioPlayer.java
@@ -1,6 +1,13 @@
 package com.plugin.cordova.audioplayer;
 
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.SoundPool;
+import android.net.rtp.AudioStream;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import org.apache.cordova.CordovaPlugin;
@@ -10,6 +17,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -17,52 +25,107 @@ import java.io.IOException;
 public class CordovaAudioPlayer extends CordovaPlugin {
 
     private static final String TAG = "CordovaAudioPlayer:Android";
-    MediaPlayer player = null;
+    private final HashMap<String, MediaPlayer> playerMaps = new HashMap<>();
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("preloadSound")) {
-            String assetKey = args.getString(0);
-            String assetPath = args.getString(1);
-            this.preloadSound(assetKey, assetPath, callbackContext);
+            String audioKey = args.getString(0);
+            String audioPath = args.getString(1);
+            this.preloadSound(audioKey, audioPath, callbackContext);
             return true;
         }
 
         if (action.equals("playSound")) {
-            String assetKey = args.getString(0);
-            this.playSound(assetKey, callbackContext);
+            String audioKey = args.getString(0);
+            this.playSound(audioKey, callbackContext);
+            return true;
+        }
+
+        if (action.equals("stopSound")) {
+            String audioKey = args.getString(0);
+            this.stopSound(audioKey, callbackContext);
             return true;
         }
 
         return false;
     }
 
-    private void playSound(String assetKey, CallbackContext callbackContext) {
-        if (assetKey.isEmpty()) {
+    private void preloadSound(String audioKey, String audioPath, CallbackContext callbackContext) {
+        if (audioKey.isEmpty() || audioPath.isEmpty()) {
+            callbackContext.error("Expected asset key or path for preloading sounds");
+        } else {
+            // Start preloading sounds
+            new Thread(() -> {
+                String fullPath = "www/".concat(audioPath);
+                Context ctx = cordova.getActivity().getApplicationContext();
+                AssetManager am = ctx.getResources().getAssets();
+                MediaPlayer player = new MediaPlayer();
+                try {
+                    AssetFileDescriptor afd = am.openFd(fullPath);
+                    player.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+                    player.prepare();
+                    player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    // player.setOnPreparedListener(mp -> {});
+                    playerMaps.put(audioKey, player);
+                    Log.d(TAG, "preloadSound: Audio key = " + audioKey + ", Audio path: " + audioPath);
+                    callbackContext.success();
+                } catch (IOException e) {
+                    Log.e(TAG, "Error preloading sound: " + e.getMessage());
+                    callbackContext.error("Error preloading sound");
+                }
+            }).start();
+        }
+    }
+
+    private void playSound(String audioKey, CallbackContext callbackContext) {
+        if (audioKey.isEmpty()) {
             callbackContext.error("Expected audio key to play an audio");
         } else {
             // Start playing audio
-            if (player != null) {
-                player.start();
+            Log.d(TAG, "playSound: Playing audio: " + audioKey);
+            MediaPlayer player1 = playerMaps.get(audioKey);
+            if (player1 != null) {
+                player1.seekTo(0);
+                player1.start();
+                player1.setOnCompletionListener(mp -> {
+                    Log.d(TAG, "playSound: Finished playing audio: " + audioKey);
+                    callbackContext.success();
+                });
             }
         }
     }
 
-    private void preloadSound(String assetKey, String assetPath, CallbackContext callbackContext) {
-        if (assetKey.isEmpty() || assetPath.isEmpty()) {
-            callbackContext.error("Expected asset key or path for preloading sounds");
+    private void stopSound(String audioKey, CallbackContext callbackContext) {
+        if (audioKey.isEmpty()) {
+            callbackContext.error("Expected audio key to stop an audio");
         } else {
-            // Start preloading sounds
-            Log.d(TAG, "preloadSound: Asset key = " + assetKey + ", Asset path: " + assetPath);
-            player = new MediaPlayer();
-            try {
-                player.setDataSource(assetPath);
-                player.prepare();
-                callbackContext.success();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            MediaPlayer player = playerMaps.get(audioKey);
+            if (player != null) {
+                player.stop();
             }
         }
+    }
+
+    private void clearMediaPlayers() {
+        for (MediaPlayer player : playerMaps.values()) {
+            if (player != null) {
+                player.release();
+            }
+        }
+        playerMaps.clear();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        clearMediaPlayers();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        clearMediaPlayers();
     }
 }
